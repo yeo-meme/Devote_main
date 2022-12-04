1043597203a0099ca543552b47d2da79960c3865
yeomeme <yeomeme@gmail.com>
2022-12-04 23:50:21 +0900

    리스트 background -> 색상 가리고 이미지 그라데이션 입히기
    UItableView -> disabled // 대체 scrollContentBackground(.hidden)

--- Devote_main/View/ContentView.swift
+++ Devote_main/View/ContentView.swift
@@ -45,12 +45,26 @@ struct ContentView: View {
         }
     }
     
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
     
     // BODY
     
     var body: some View {
         NavigationView {
-            
             ZStack {
                 VStack{
                     VStack(spacing: 16){
@@ -73,68 +87,48 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .background(isButtonDisabled ? Color.gray : Color.pink)
                         .cornerRadius(10)
-                    }//: vstack
+                    }//: TEXT VSTACK - 입력란
                     .padding()
                     
                     
-                    VStack {
-                        List {
-                            ForEach(items) { item in
-                                NavigationLink {
-                                    VStack(alignment: .leading) {
-                                        Text(item.task ?? "")
-                                            .font(.headline)
-                                            .fontWeight(.bold)
-                                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                            .font(.footnote)
-                                            .foregroundColor(.gray)
-                                    }// : LIST ITEM
-                                } label: {
-                                    Text(item.timestamp!, formatter: itemFormatter)
-                                }
+                    List {
+                        ForEach(items) { item in
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
                             }
-                            .onDelete(perform: deleteItems)
-                        }//: List
-                            .listStyle(InsetGroupedListStyle())
-                            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
-                            .padding(.vertical, 0)
-                            .frame(maxWidth: 640)
-                    }//: Vsstack
-                }//: zstack
-                .onAppear(){
-                    UITableView.appearance().backgroundColor = UIColor.clear
-                }
-                .navigationBarTitle("Daily Tasks", displayMode: .large )
-                .toolbar {
-                    #if os(iOS)
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        EditButton()
-                    }
-                    #endif
-                } //: Toolbar
-                .background(
-                    backgroundGradient.ignoresSafeArea(.all))
-            }//: navigation
-            .navigationViewStyle(StackNavigationViewStyle())
-        }
+                        }// : LIST ITEM
+                        .onDelete(perform: deleteItems)
+                    }//: LIST
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
+                }//: VSTACK
+            } // : ZSTACK
+            .scrollContentBackground(.hidden)
         
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
+            .navigationBarTitle("Daily Tasks", displayMode: .large )
+            .toolbar {
+#if os(iOS)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                } //:TOOLBAR
+#endif
+            } //: Toolbar
+            .background(
+              BackgroundImageView()
+            )
+            .background(
+                backgroundGradient.ignoresSafeArea(.all))
+        }//: navigation
+        .navigationViewStyle(StackNavigationViewStyle())
+    } //:VIEW
+} // contView
 
 
 struct ContentView_Previews: PreviewProvider {
