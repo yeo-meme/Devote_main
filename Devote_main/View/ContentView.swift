6c41bfe0a113467b3f7ea29b4b8640b3ff62a02b
yeomeme <yeomeme@gmail.com>
2022-12-27 17:08:44 +0900

    [add] 할일 newTaskItemView 로 분리 , edit button 및 newtask 버튼 추가

--- Devote_main/View/ContentView.swift
+++ Devote_main/View/ContentView.swift
@@ -10,7 +10,10 @@ import CoreData
 
 struct ContentView: View {
     // MARK: - PROPERTY
+    
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @State var task : String = ""
+    @State private var showNewTaskItem : Bool = false
     
     private var isButtonDisabled : Bool{ task.isEmpty
     }
@@ -66,29 +69,64 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 VStack{
-                    VStack(spacing: 16){
-                        TextField("New Task", text: $task)
-                            .padding()
-                            .background(
-                                Color(UIColor.systemGray6)
-                            )
-                            .cornerRadius(10)
+                    HStack(spacing: 10) {
+                        Text("Devote")
+                            .font(.system(.largeTitle
+                                          ,design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding(.leading, 4)
                         
+                        Spacer()
+                        
+                        //EditButton
+                        EditButton()
+                            .font(.system(size: 16,
+                                          weight: .semibold,
+                                          design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(Capsule().stroke(Color.white, lineWidth: 2))
+                        
+                        // apperance button
                         Button(action: {
-                            addItem()
-                        }, label:{ Spacer()
-                            Text("SAVE")
-                            Spacer()
+                            //toggle appearance
+                            isDarkMode.toggle()
+                        }, label: {
+                            Image(systemName: isDarkMode ?
+                                  "moon.circle.fill" : "moon.circle")
+                            .resizable()
+                            .frame(width: 24, height:24)
+                            .font(.system(.title, design: .rounded))
                         })
-                        .disabled(isButtonDisabled)
-                        .padding()
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .background(isButtonDisabled ? Color.gray : Color.pink)
-                        .cornerRadius(10)
-                    }//: TEXT VSTACK - 입력란
+                    } //HSTACK
                     .padding()
+                    .foregroundColor(.white)
                     
+                    Spacer(minLength: 80)
+//
+                    
+                    // MARK: - new task button
+                    
+                    Button(action: {
+                        showNewTaskItem = true
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 30,
+                                          weight: .semibold,
+                                          design: .rounded))
+                        Text("New Task")
+                            .font(.system(size: 24, weight: .bold
+                                          , design: .rounded))
+                    })
+                    .foregroundColor(.white)
+                    .padding(.horizontal , 20)
+                    .padding(.vertical, 15)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.pink,Color.blue]), startPoint: .leading, endPoint: .trailing))
+                    .clipShape(Capsule())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 8, x: 0.0, y: 4.0)
+                    
+                    //mark - tasks
                     
                     List {
                         ForEach(items) { item in
@@ -103,7 +141,10 @@ struct ContentView: View {
                         }// : LIST ITEM
                         .onDelete(perform: deleteItems)
                     }//: LIST
-                    .listStyle(InsetGroupedListStyle())
+//                    .listStyle(InsetGroupedListStyle())
+                    .cornerRadius(20)
+                    .listStyle(.insetGrouped)
+                    .padding(20)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
@@ -111,14 +152,14 @@ struct ContentView: View {
             } // : ZSTACK
             .scrollContentBackground(.hidden)
         
-            .navigationBarTitle("Daily Tasks", displayMode: .large )
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                } //:TOOLBAR
-#endif
-            } //: Toolbar
+//            .navigationBarTitle("Daily Tasks", displayMode: .large )
+//            .toolbar {
+//#if os(iOS)
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                } //:TOOLBAR
+//#endif
+//            } //: Toolbar
             .background(
               BackgroundImageView()
             )
